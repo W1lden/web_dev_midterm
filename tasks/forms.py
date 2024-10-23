6c733b0590b8e382179fcb619f4364780ab5a461
Асанали Битанов <asan@MacBook-Air-Asanali.local>
2024-10-23 22:37:55 +0500

    final update

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Task
 
 class TaskForm(forms.ModelForm):
+    due_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        required=False
+    )
+
     class Meta:
         model = Task
-        fields = ['title', 'description', 'completed']
+        fields = ['title', 'description', 'completed', 'due_date']
